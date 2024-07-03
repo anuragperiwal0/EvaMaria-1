3b5b0a23650d84e74b637eb9c8e0231df68831cb
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2024-07-03 20:12:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -374,7 +374,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer("നിങ്ങളുടെ ബുദ്ധി കൊള്ളാം,പക്ഷെ അത് എന്റെ അടുത്ത് വേണ്ട😹,ജോയിൻ ആകാതെ ഒന്നും നടക്കില്ല😇", show_alert=True)
+            await query.answer("നിങ്ങളുടെ ബുദ്ധി വിമാനം തന്നെ🤭,പക്ഷെ അത് എന്റെ അടുത്ത് വേണ്ട😹,ജോയിൻ ആകാതെ ഒന്നും നടക്കില്ല😇", show_alert=True)
             return
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
